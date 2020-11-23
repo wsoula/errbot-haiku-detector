68f513b2b96cf8989f74b4ce2a8b0cb67f3bc884
Will Soula <william.soula@starz.com>
2020-11-22 22:19:37 -0700

    Add some debugging

--- haikudetector.py
+++ haikudetector.py
@@ -15,18 +15,19 @@ class Haikudetector(BotPlugin):
         line_3 = ''
         line_3_syllables = 0
         syllable_count = 0
+        print(body_array)
         for word in body_array:
             word_syllables = syllables.estimate(word)
             syllable_count = syllable_count + word_syllables
             if line_1_syllables + word_syllables <= 5:
                 line_1_syllables = line_1_syllables + word_syllables
-                line_1 = line_1 + word
+                line_1 = line_1 + ' ' + word
             elif line_2_syllables + word_syllables <= 7 and line_1_syllables == 5:
                 line_2_syllables = line_2_syllables + word_syllables
-                line_2 = line_2 + word
+                line_2 = line_2 + ' ' + word
             elif line_3_syllables + word_syllables <= 5 and line_1_syllables == 5 and line_2_syllables == 7:
                 line_3_syllables = line_3_syllables + word_syllables
-                line_3 = line_3 + word
+                line_3 = line_3 + ' ' + word
         if syllable_count == 17 and mess.body.upper().replace(' ', '').find('FII') != -1:
             return 'You said a haikui\n'+line_1+'\n'+line_2+'\n'+line_3
 
