ccf8585f66ab53842ac0c53146ff76b93671bd42
Will Soula <william.soula@starz.com>
2020-11-22 20:47:12 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+errbot-haiku-detector
+---
+
+Looks for responses that are haikus and returns them formatted as a haiku.
+Based on the reddit haiku bot
---
+++ haikudetector.py
@@ -0,0 +1,11 @@
+from errbot import BotPlugin, botcmd, arg_botcmd, re_botcmd
+import random
+import syllables
+
+
+class Haikudetector(BotPlugin):
+    """Detects responses with haikus and returns a formatted haiku"""
+    def callback_message(self, mess):
+        """Runs on every message"""
+        if syllables.estimate(mess.body) == 17 and mess.body.upper().replace(' ', '').find('FOO') != -1:
+            return "You said a haiku"
