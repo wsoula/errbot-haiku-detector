310164cb78946e0c9b67722f33589430f7a3e351
Will Soula <spam@wsoula.info>
2022-03-09 07:58:32 -0700

    feat: Add ability to only add a reaction, log haiku

--- haikudetector.py
+++ haikudetector.py
@@ -1,39 +1,56 @@
-from errbot import BotPlugin, botcmd, arg_botcmd, re_botcmd
-import syllables
+""" Detect when people talk in haiku """
+import os
 import logging
+from errbot import BotPlugin, arg_botcmd
+import syllables
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+MODE = os.getenv('MODE', 'reaction')  # 'reaction' for just an emoji, 'message' for a message to be sent, 'all' for all
+REACTION = os.getenv('REACTION', 'haiku')  # https://slackmojis.com/emojis/3545-haiku
+PREFIX = os.getenv('PREFIX', '!')  # Only run for one tobor, if you have multiple with different prefixes
 
 
 class Haikudetector(BotPlugin):
-    """Detects responses with haikus and returns a formatted haiku"""
+    """ Detects responses with haikus and returns a formatted haiku """
     def callback_message(self, mess):
         """Runs on every message"""
-        body_array = mess.body.replace('  ', ' ').split(' ')
-        line_1 = ''
-        line_1_syllables = 0
-        line_2 = ''
-        line_2_syllables = 0
-        line_3 = ''
-        line_3_syllables = 0
-        syllable_count = 0
-        for word in body_array:
-            word_syllables = syllables.estimate(word)
-            syllable_count = syllable_count + word_syllables
-            if line_1_syllables + word_syllables <= 5:
-                line_1_syllables = line_1_syllables + word_syllables
-                line_1 = line_1 + ' ' + word
-            elif line_2_syllables + word_syllables <= 7 and line_1_syllables == 5:
-                line_2_syllables = line_2_syllables + word_syllables
-                line_2 = line_2 + ' ' + word
-            elif line_3_syllables + word_syllables <= 5 and line_1_syllables == 5 and line_2_syllables == 7:
-                line_3_syllables = line_3_syllables + word_syllables
-                line_3 = line_3 + ' ' + word
-        if syllable_count == 17 and line_1_syllables == 5 and line_2_syllables == 7 and line_3_syllables == 5:
-            self.send_card(
-                  in_reply_to=mess,
-                  body=line_1+'\n'+line_2+'\n'+line_3,
-                  title='You might have said a haiku!'
-              )
+        bot_prefix = os.getenv('BOT_PREFIX')
+        if PREFIX == bot_prefix:
+            body_array = mess.body.replace('  ', ' ').split(' ')
+            line_1 = ''
+            line_1_syllables = 0
+            line_2 = ''
+            line_2_syllables = 0
+            line_3 = ''
+            line_3_syllables = 0
+            syllable_count = 0
+            for word in body_array:
+                word_syllables = syllables.estimate(word)
+                syllable_count = syllable_count + word_syllables
+                if line_1_syllables + word_syllables <= 5:
+                    line_1_syllables = line_1_syllables + word_syllables
+                    line_1 = line_1 + ' ' + word
+                elif line_2_syllables + word_syllables <= 7 and line_1_syllables == 5:
+                    line_2_syllables = line_2_syllables + word_syllables
+                    line_2 = line_2 + ' ' + word
+                elif line_3_syllables + word_syllables <= 5 and line_1_syllables == 5 and line_2_syllables == 7:
+                    line_3_syllables = line_3_syllables + word_syllables
+                    line_3 = line_3 + ' ' + word
+            if syllable_count == 17 and line_1_syllables == 5 and line_2_syllables == 7 and line_3_syllables == 5:
+                haiku_text = line_1+'\n'+line_2+'\n'+line_3
+                if MODE in ['message', 'all']:
+                    self.send_card(
+                        in_reply_to=mess, body=haiku_text, title='You might have said a haiku!')
+                if MODE in ['reaction', 'all']:
+                    self._bot.add_reaction(mess, REACTION)
+                    logger.info('haiku=%s', haiku_text)
+        else:
+            logger.info('prefix=%s bot_prefix=%s not running haiku code', PREFIX, bot_prefix)
 
     @arg_botcmd('word', type=str)
     def syllables(self, msg, word=None):
+        """ Return the syllable count for a word """
+        logger.info('msg=%s', msg)
         return syllables.estimate(word)
